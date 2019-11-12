893a8f712161e966ebd1e8b4ef454576c53ed427
Kail Amoakon <kamoako1@uncc.edu>
2019-11-12 04:07:46 +0000

    Third Commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+
+        sessions[:user_id] = user.id 
+
+        redirect_to '/welcome'
+
+    end
   end
 
   def login
