72c80ee9b537a7eeb93985ebef0fcf652cb8fbc6
Kail Amoakon <kamoako1@uncc.edu>
2019-11-14 18:24:57 +0000

    Commiting updates

---
+++ db/migrate/20191112215613_add_email_to_users.rb
@@ -0,0 +1,8 @@
+class AddEmailToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :email, :string
+    add_index :users, :email, unique: true
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_030414) do
+ActiveRecord::Schema.define(version: 2019_11_12_215613) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "firstname"
+    t.string "lastname"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
