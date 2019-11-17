d6960172488e03cbaa2161c4d5cf8473dd8541b9
Kail Amoakon <kamoako1@uncc.edu>
2019-11-17 03:37:23 +0000

    Ad Delete functionality for lists page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,8 +24,15 @@ class ListsController < ApplicationController
             render 'edit' 
         end 
     end
+    def destroy  
+        @list = List.find(params[:id])
+        @list.destroy
+        
+        redirect_to lists_path 
+    end 
+end
 private
     def list_params
         params.require(:list).permit(:product,:store)
     end
-end
+
