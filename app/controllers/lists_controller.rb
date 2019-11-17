8b23f8664a413f44b10fb4080ed2353e9984e68e
Kail Amoakon <kamoako1@uncc.edu>
2019-11-17 02:27:11 +0000

    Create the new action and view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,2 @@
 class ListsController < ApplicationController
-    def new
-    end
-    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_215613) do
+ActiveRecord::Schema.define(version: 2019_11_16_185704) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "username"
+    t.string "product"
+    t.string "store"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
