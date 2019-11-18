08444c33b436bde7af005de24c7fc355f597e703
Kail Amoakon <kamoako1@uncc.edu>
2019-11-18 04:21:05 +0000

    Working on dropdown for price

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -33,6 +33,6 @@ class ListsController < ApplicationController
 end
 private
     def list_params
-        params.require(:list).permit(:user_id,:product,:store)
+        params.require(:list).permit(:user_id,:product,:store,:price)
     end
 
