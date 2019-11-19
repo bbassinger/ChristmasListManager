c80630381a62238624fbb8a20041b30a2794aebe
Kail Amoakon <kamoako1@uncc.edu>
2019-11-19 20:40:57 +0000

    Adding user reference to the list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_16_185704) do
+ActiveRecord::Schema.define(version: 2019_11_19_203902) do
 
   create_table "lists", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_11_16_185704) do
     t.string "store"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
