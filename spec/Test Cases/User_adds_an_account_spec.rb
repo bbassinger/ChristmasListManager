8cf9bdd8f73d9e2c76942a685909898db15b4db3
Brooke Bassinger <bbassin2@uncc.edu>
2019-11-19 22:21:54 +0000

    test case 4 - user navigates to their christmas list

--- spec/Test Cases/User_adds_an_account_spec.rb
+++ spec/Test Cases/User_adds_an_account_spec.rb
@@ -39,12 +39,16 @@ feature "User logs out successfuly" do
 end
 
 
-#feature "User adds a product to the christmas list" do	scenario "User successfully adds to the christmas list" do
-#		visit welcome_path
-#		expect(page).to have_content("RETURNING USER")
-#		fill_in "username", with: "bbassin"
-#		fill_in "password", with: "12345"
-#		click_button "Login"
-##		
-#	end
-#end
+feature "User navigates to their list" do	
+    scenario "User successfully navigated to their christmas list" do
+		visit welcome_path
+		expect(page).to have_content("RETURNING USER")
+        click_link "RETURNING USER"
+		fill_in "username", with: "bbassin"
+		fill_in "password", with: "12345"
+		click_button "Login"
+		expect(page).to have_content("Add to your Christmas List!")
+		click_link "My List"
+		expect(page).to have_content("Your Christmas list!")
+	end
+end
