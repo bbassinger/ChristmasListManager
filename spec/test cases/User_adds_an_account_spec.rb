e8579e92093826baa66fe78db205022a6609dabc
Brooke Bassinger <bbassin2@uncc.edu>
2019-11-19 00:54:21 +0000

    Added third test case - user logs out successfully

--- spec/test cases/User_adds_an_account_spec.rb
+++ spec/test cases/User_adds_an_account_spec.rb
@@ -8,7 +8,7 @@ feature "User logs in" do
 	    fill_in "username", with: "bbassin"
 	    fill_in "password", with: "12345"
 	    click_button "Login"
-	    #expect(page).to have_content("You are Logged In, bbassin")
+	    expect(page).to have_content("Add to your Christmas List!")
 	    
 	end
 end
@@ -24,6 +24,21 @@ feature "User creates an account" do
 		click_button "Create User"
 	end	
 end
+
+feature "User logs out successfuly" do
+	scenario "User is able to log out after signing in" do
+		visit welcome_path
+	    expect(page).to have_content("RETURNING USER")
+	    click_link "RETURNING USER"
+	    fill_in "username", with: "bbassin"
+	    fill_in "password", with: "12345"
+	    click_button "Login"
+	    expect(page).to have_content("Add to your Christmas List!")
+	    click_link "Log Out"
+	    expect(page).to have_content("RETURNING USER")
+	end
+end
+
 
 #feature "User adds a product to the christmas list" do
 #	scenario "User successfully adds to the christmas list" do
