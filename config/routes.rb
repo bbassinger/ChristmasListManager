5f296f2dc1848917f6eddce5d383bf6ad323990d
Brooke Bassinger <bbassin2@uncc.edu>
2019-11-19 00:39:02 +0000

    Added a Log Out link to the list view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :destroy]
   resources :lists
   get 'login', to: 'sessions#new'
 
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   get 'welcome', to: 'sessions#welcome'
 
   get 'authorized', to: 'sessions#page_requires_login'
-
+  
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sessions#welcome'
